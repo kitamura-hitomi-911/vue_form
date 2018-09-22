75c92935ad9e3281400fb7fe98dace48f723823c
hitomi <hinitial@gmail.com>
2018-09-23 00:10:22 +0900

    webpack-simple で 単一ファイルコンポーネントテスト

---
+++ src/components/vmForm.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="form">
+		aa
+	</div>
+</template>
+<script>
+	export default {
+		name:'vmForm',
+		data:function(){
+			return {
+				hoge:'hoge'
+			}
+		}
+	};
+</script>
+<style>
+	.form{
+		color:#f00;
+	}
+</style>
